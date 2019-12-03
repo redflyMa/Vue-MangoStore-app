5c954ad51260c927fbb60f41f282e73e8e6492a5
redfly <123@qq.com>
2019-12-03 20:07:05 +0800

    text

--- vue-mango-store/src/views/Home.vue
+++ vue-mango-store/src/views/Home.vue
@@ -1,6 +1,7 @@
 <template>
   <div class="home">
     <img alt="Vue logo" src="../assets/logo.png">
+    <span>hhhhhhhhh</span>
     <HelloWorld msg="Welcome to Your Vue.js App"/>
   </div>
 </template>
